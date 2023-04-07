21399777f4eb20d6bb649a57221610e9a13a4695
sudip-tagline <129746521+sudip-tagline@users.noreply.github.com>
2023-04-07 18:11:21 +0530

    new files added

---
+++ README.md
@@ -0,0 +1,15 @@
+# flask-location-api
+flask location api with searching
+
+Prefer virtualenv
+
+For requirements
+pip install -r requirements.txt
+
+To Run the Flask Program
+flask --app app run --debug
+OR
+flask run
+
+For testing the api
+http://localhost:5000/location/?search=Mumbai
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify
+import sqlite3
+import json
+import csv
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET'])
+
+def hello():
+    return "Hello Flask!"
+
+
+@app.route('/location/', methods=['GET'])
+def search_location():
+    search = request.args.get('search')
+    print("search::::::::**********", search)
+    db_path = os.environ.get("DATABASE_PATH")
+    with sqlite3.connect(db_path) as location:
+        cursor = location.cursor()
+        cursor.execute(f'SELECT DISTINCT zc.code, zc.city, zc.lat, zc.lon, st.name as state_name, coun.name as country_name FROM zipcodes zc INNER JOIN states st \
+                       ON zc.state_id=st.id INNER JOIN countries coun ON coun.id=st.country_id INNER JOIN counties cout ON cout.state_id=st.id\
+                       WHERE zc.city LIKE "%{search}%" or st.name LIKE "%{search}%" or coun.name LIKE "%{search}%"')
+        row_headers=[x[0] for x in cursor.description]
+        rv = cursor.fetchall()
+        json_data=[]
+        for result in rv:
+                json_data.append(dict(zip(row_headers, result)))
+    return json.dumps(json_data)
+    
+  
+
+if __name__ == '__main__':
+    app.run()
